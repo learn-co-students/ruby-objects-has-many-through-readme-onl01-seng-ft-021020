de759b4207a1a27b0ad8354a9f9fb344e49a9ed7
Victor Ibasco <victoribasco18@gmail.com>
2020-02-13 21:16:48 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,6 +18,7 @@ class Waiter
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
     end
+<<<<<<< HEAD
     best_tipped_meal.customer
   end
  
@@ -31,4 +32,10 @@ class Waiter
     end
   end
  
+=======
+   
+    best_tipped_meal.customer
+  end
+ 
+>>>>>>> 9ba0b8146c845efb6008e0d87c630b058d2483b8
 end
